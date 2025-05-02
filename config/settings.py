ee6b4bef3242e5b1cbcc3cda56d88d6ee1bb3449
Oscar Nadjar <oscar.nadjar@moodle.com>
2025-05-01 19:34:56 -0500

    Improving UI

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
